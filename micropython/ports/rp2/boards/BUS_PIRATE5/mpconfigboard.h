86dc2b67ecc36f62aa5176b548a65e64042816b8
Chris Lott <rclott@hiwaay.net>
2024-09-04 15:47:46 +0900

    added MP board definition file

---
+++ micropython/ports/rp2/boards/BUS_PIRATE5/mpconfigboard.h
@@ -0,0 +1,9 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Bus Pirate 5 RP2040"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+// #define MICROPY_HW_FLASH_STORAGE_BYTES          (8 * 1024 * 1024)
+
+// Define flash label (this doesn't work on RP2 port)
+// #define MICROPY_HW_FLASH_FS_LABEL     "rp5"
+// Whether to include doc strings (increases RAM usage)
+#define MICROPY_ENABLE_DOC_STRING (1)
